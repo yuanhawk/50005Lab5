ff1a9afa39a6fe2e388d0bf6aa14f05e577121ed
Tan Li Yuan Marcus <yuanmirage@yahoo.com>
2021-04-04 21:33:40 +0800

    part 2 completed

--- EncryptionLab/DesImageSolution.java
+++ EncryptionLab/DesImageSolution.java
@@ -1,18 +1,89 @@
-import java.lang.Object;
 import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import java.nio.*;
 import javax.crypto.*;
-import java.util.Base64;
 
 
 public class DesImageSolution {
-    public static void main(String[] args) throws Exception{
+
+    private static final String
+            CBC = "CBC",
+            SUTD = "SUTD.bmp",
+            TRIANGLE = "triangle.bmp";
+
+    public static void main(String[] args) {
+        try {
+            encryptImage(SUTD, DesSolution.ECB);
+            encryptImage(TRIANGLE, DesSolution.ECB);
+            encryptImage(SUTD, CBC);
+            encryptImage(TRIANGLE, CBC);
+            encryptBottomUpImage(TRIANGLE, DesSolution.ECB);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void encryptBottomUpImage(String file, String type) throws IOException {
         int image_width = 200;
         int image_length = 200;
         // read image file and save pixel value into int[][] imageArray
-        BufferedImage img = ImageIO.read(new File("SUTD.bmp"));
+        BufferedImage img = ImageIO.read(new File(String.format("%s%s", DesSolution.DIRECTORY, file)));
+        image_width = img.getWidth();
+        image_length = img.getHeight();
+        // byte[][] imageArray = new byte[image_width][image_length];
+        int[][] imageArray = new int[image_width][image_length];
+        for(int idx = image_width - 1; idx > -1; idx--) {
+            for(int idy = image_length - 1; idy > -1; idy--) {
+                int color = img.getRGB(idx, idy);
+                imageArray[idx][idy] = color;
+            }
+        }
+
+        DesSolution desSoln = new DesSolution();
+        // TODO: generate secret key using DES algorithm
+        SecretKey key = desSoln.generateSecretKey();
+
+        // TODO: Create cipher object, initialize the ciphers with the given key, choose encryption algorithm/mode/padding,
+        Cipher cipher = desSoln.initCipher(type, Cipher.ENCRYPT_MODE, key);
+
+        //you need to try both ECB and CBC mode, use PKCS5Padding padding method
+
+        // define output BufferedImage, set size and format
+        BufferedImage outImage = new BufferedImage(image_width, image_length, BufferedImage.TYPE_3BYTE_BGR);
+
+        for(int idx = image_width - 1; idx > -1; idx--) {
+            // convert each column int[] into a byte[] (each_width_pixel)
+            byte[] each_width_pixel = new byte[4*image_length];
+            for(int idy = image_length - 1; idy > -1; idy--) {
+                ByteBuffer dbuf = ByteBuffer.allocate(4);
+                dbuf.putInt(imageArray[idx][idy]);
+                byte[] bytes = dbuf.array();
+                System.arraycopy(bytes, 0, each_width_pixel, idy*4, 4);
+            }
+            // TODO: encrypt each column or row bytes
+            byte[] encryptedBytesArray = desSoln.generateByte(cipher, each_width_pixel);
+
+            // TODO: convert the encrypted byte[] back into int[] and write to outImage (use setRGB)
+            int[] arr = new int[encryptedBytesArray.length];
+            for (int i = encryptedBytesArray.length - 1; i > -1; i--) {
+                arr[i] = encryptedBytesArray[i];
+            }
+
+            for (int i = image_length - 1; i > -1; i--) {
+                outImage.setRGB(idx, i, arr[i]);
+            }
+        }
+
+        //write outImage into file
+        ImageIO.write(outImage, "BMP",new File("triangle_new.bmp"));
+    }
+
+    private static void encryptImage(String file, String type) throws IOException {
+        int image_width = 200;
+        int image_length = 200;
+        // read image file and save pixel value into int[][] imageArray
+        BufferedImage img = ImageIO.read(new File(String.format("%s%s", DesSolution.DIRECTORY, file)));
         image_width = img.getWidth();
         image_length = img.getHeight();
         // byte[][] imageArray = new byte[image_width][image_length];
@@ -20,21 +91,24 @@ public class DesImageSolution {
         for(int idx = 0; idx < image_width; idx++) {
             for(int idy = 0; idy < image_length; idy++) {
                 int color = img.getRGB(idx, idy);
-                imageArray[idx][idy] = color;            
+                imageArray[idx][idy] = color;
             }
-        } 
-// TODO: generate secret key using DES algorithm
+        }
 
+        DesSolution desSoln = new DesSolution();
+        // TODO: generate secret key using DES algorithm
+        SecretKey key = desSoln.generateSecretKey();
 
-// TODO: Create cipher object, initialize the ciphers with the given key, choose encryption algorithm/mode/padding,
-//you need to try both ECB and CBC mode, use PKCS5Padding padding method
-        
+        // TODO: Create cipher object, initialize the ciphers with the given key, choose encryption algorithm/mode/padding,
+        Cipher cipher = desSoln.initCipher(type, Cipher.ENCRYPT_MODE, key);
+
+        //you need to try both ECB and CBC mode, use PKCS5Padding padding method
 
         // define output BufferedImage, set size and format
-        BufferedImage outImage = new BufferedImage(image_width,image_length, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage outImage = new BufferedImage(image_width, image_length, BufferedImage.TYPE_3BYTE_BGR);
 
         for(int idx = 0; idx < image_width; idx++) {
-        // convert each column int[] into a byte[] (each_width_pixel)
+            // convert each column int[] into a byte[] (each_width_pixel)
             byte[] each_width_pixel = new byte[4*image_length];
             for(int idy = 0; idy < image_length; idy++) {
                 ByteBuffer dbuf = ByteBuffer.allocate(4);
@@ -42,14 +116,21 @@ public class DesImageSolution {
                 byte[] bytes = dbuf.array();
                 System.arraycopy(bytes, 0, each_width_pixel, idy*4, 4);
             }
-// TODO: encrypt each column or row bytes 
-            
+            // TODO: encrypt each column or row bytes
+            byte[] encryptedBytesArray = desSoln.generateByte(cipher, each_width_pixel);
 
-// TODO: convert the encrypted byte[] back into int[] and write to outImage (use setRGB)
-            
-            
+            // TODO: convert the encrypted byte[] back into int[] and write to outImage (use setRGB)
+            int[] arr = new int[encryptedBytesArray.length];
+            for (int i = 0; i < encryptedBytesArray.length; i++) {
+                arr[i] = encryptedBytesArray[i];
+            }
+
+            for (int i = 0; i < image_length; i++) {
+                outImage.setRGB(idx, i, arr[i]);
+            }
         }
-//write outImage into file
-        ImageIO.write(outImage, "BMP",new File("EnSUTD.bmp"));
+
+        //write outImage into file
+        ImageIO.write(outImage, "BMP",new File(String.format("En%s%s", type, file)));
     }
 }
--- EncryptionLab/DesSolution.java
+++ EncryptionLab/DesSolution.java
@@ -7,9 +7,9 @@ import java.util.Base64;
 
 public class DesSolution {
 
-    private static final String
+    public static final String
             ENCRYPTION = "DES",
-            MODE = "ECB",
+            ECB = "ECB",
             BLOCK_SIZE = "PKCS5Padding",
             DIRECTORY = "./EncryptionLab/",
             SHORTTEXT = "shorttext.txt",
@@ -29,11 +29,11 @@ public class DesSolution {
         desEncrypt(LONGTEXT,sb2);
     }
 
-    private StringBuilder initStringFromDir(String dir) {
+    private StringBuilder initStringFromDir(String file) {
         StringBuilder text = new StringBuilder();
         String line;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(String.format("%s%s", DIRECTORY, dir)));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(String.format("%s%s", DIRECTORY, file)));
             while ((line = bufferedReader.readLine()) != null) {
                 text.append("\n").append(line);
             }
@@ -43,15 +43,15 @@ public class DesSolution {
         return text;
     }
 
-    private static void desEncrypt(String dir, StringBuilder sb) {
+    private static void desEncrypt(String file, StringBuilder sb) {
         SecretKey desKey = desSoln.generateSecretKey();
 
-        Cipher desCipher = desSoln.initCipher(Cipher.ENCRYPT_MODE, desKey);
+        Cipher desCipher = desSoln.initCipher(ECB, Cipher.ENCRYPT_MODE, desKey);
 
         byte[] encryptedBytesArray = desSoln.generateEncryptedByte(desCipher, sb.toString());
         if (encryptedBytesArray != null) {
             //TODO: print the length of output encrypted byte[], compare the length of file shorttext.txt and longtext.txt
-            System.out.printf("Length of %s is %d\n%n", dir, encryptedBytesArray.length);
+            System.out.printf("Length of %s is %d\n", file, encryptedBytesArray.length);
 
             //TODO: Question 3
             System.out.println(new String(encryptedBytesArray));
@@ -60,11 +60,11 @@ public class DesSolution {
             String base64format = Base64.getEncoder().encodeToString(encryptedBytesArray);
 
             //TODO: print the encrypted message (in base64format String format)
-            System.out.println("Encrypted message content:");
+            System.out.println("\nEncrypted message content:");
             System.out.println(base64format);
 
             //TODO: create cipher object, initialize the ciphers with the given key, choose decryption mode as DES
-            Cipher desCipher2 = desSoln.initCipher(Cipher.DECRYPT_MODE, desKey);
+            Cipher desCipher2 = desSoln.initCipher(ECB, Cipher.DECRYPT_MODE, desKey);
 
             //TODO: do decryption, by calling method Cipher.doFinal().
             byte[] decryptedBytesArray = desSoln.generateByte(desCipher2, encryptedBytesArray);
@@ -74,12 +74,13 @@ public class DesSolution {
 
                 //TODO: print the decrypted String text and compare it with original text
                 System.out.println(decryptedString);
+                System.out.println();
             }
         }
     }
 
     //TODO: generate secret key using DES algorithm
-    private SecretKey generateSecretKey() {
+    public SecretKey generateSecretKey() {
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance(ENCRYPTION);
             return keyGen.generateKey();
@@ -90,9 +91,9 @@ public class DesSolution {
     }
 
     //TODO: create cipher object, initialize the ciphers with the given key, choose encryption mode as DES
-    private Cipher initCipher(int mode, SecretKey key) {
+    public Cipher initCipher(String type, int mode, SecretKey key) {
         try {
-            Cipher desCipher = Cipher.getInstance(String.format("%s/%s/%s", ENCRYPTION, MODE, BLOCK_SIZE));
+            Cipher desCipher = Cipher.getInstance(String.format("%s/%s/%s", ENCRYPTION, type, BLOCK_SIZE));
             desCipher.init(mode, key);
             return desCipher;
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
@@ -105,7 +106,7 @@ public class DesSolution {
         return generateByte(c, s.getBytes());
     }
 
-    private byte[] generateByte(Cipher c, byte[] b) {
+    public byte[] generateByte(Cipher c, byte[] b) {
         if (c != null && b != null) {
             //TODO: do encryption, by calling method Cipher.doFinal().
             try {
